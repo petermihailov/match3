86c15fac2f80893eb10ab44d71b6293011f859c7
Peter Mihailov <usernameispeter@gmail.com>
2018-04-04 22:14:29 +0300

    add production / development build

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,75 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: ['babel-polyfill', './src/main.js'],
-  module: {
-    rules: [
+const scss = {
+  production: {
+    test: /\.s?css$/,
+    use: [
+      MiniCssExtractPlugin.loader,
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        loader: "css-loader",
+        options: {
+          modules: true,
+          localIdentName: '[hash:base64:5]',
+          minimize: true,
+          sourceMap: false
         }
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: {minimize: false}
-          }
-        ]
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
-              minimize: false,
-              sourceMap: true
-            }
-          },
-          "postcss-loader"
-        ]
-      }
+      "postcss-loader"
     ]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-      hash: true
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+  development: {
+    test: /\.s?css$/,
+    use: [
+      'style-loader',
+      {
+        loader: "css-loader",
+        options: {
+          modules: true,
+          localIdentName: '[path][name]__[local]--[hash:base64:5]',
+          minimize: false,
+          sourceMap: true
+        }
+      },
+      "postcss-loader"
+    ]
+  }
+};
+
+module.exports = (env, argv) => {
+  return ({
+    entry: ['babel-polyfill', './src/main.js'],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: {minimize: false}
+            }
+          ]
+        },
+        scss[argv.mode]
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html",
+        hash: true
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ]
+  });
 };
