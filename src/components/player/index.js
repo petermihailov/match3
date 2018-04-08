eb14ea1912f07ae31618f129d04d6e23b4812439
Peter Mihailov <usernameispeter@gmail.com>
2018-04-08 15:16:40 +0300

    animate points

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import anime from 'animejs';
 import cn from 'classnames';
 import styles from './player.scss';
 
 export default class Player extends Component {
+  componentWillReceiveProps({score}) {
+    if (score !== this.props.score) {
+      const animatedScore = {value: this.props.score};
+
+      anime({
+        targets: animatedScore,
+        value: score,
+        round: 1,
+        easing: 'easeInOutExpo',
+        duration: Math.abs(score - this.props.score) / 5,
+        update: () => this.score.innerText = animatedScore.value
+      });
+    }
+  }
+
   render() {
-    const {name, score, right} = this.props;
+    const {name, right} = this.props;
 
     return (
       <div className={cn(
@@ -14,7 +30,7 @@ export default class Player extends Component {
         }
       )}>
         <span className={styles.name}>{name}</span>
-        <span className={styles.score}>{(score).toLocaleString('en')}</span>
+        <span ref={(n) => this.score = n} className={styles.score}>0</span>
       </div>
     );
   }
