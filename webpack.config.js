b868232953d5813f8d4d99974d9993e0accb72aa
Peter Mihailov <usernameispeter@gmail.com>
2018-11-26 21:41:03 +0300

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const scss = {
   production: {
@@ -8,40 +8,40 @@ const scss = {
     use: [
       MiniCssExtractPlugin.loader,
       {
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           modules: true,
           localIdentName: '[hash:base64:5]',
           minimize: true,
-          sourceMap: false
-        }
+          sourceMap: false,
+        },
       },
-      "postcss-loader"
-    ]
+      'postcss-loader',
+    ],
   },
   development: {
     test: /\.s?css$/,
     use: [
       'style-loader',
       {
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           modules: true,
           localIdentName: '[path][name]__[local]--[hash:base64:5]',
           minimize: false,
-          sourceMap: true
-        }
+          sourceMap: true,
+        },
       },
-      "postcss-loader"
-    ]
-  }
+      'postcss-loader',
+    ],
+  },
 };
 
 module.exports = (env, argv) => {
   return ({
-    entry: ['babel-polyfill', './src/main.js', './src/reset.css', './src/colors.css', './src/main.css'],
+    entry: ['@babel/polyfill', './src/main.js', './src/reset.css', './src/colors.css', './src/main.css'],
     output: {
-      path: __dirname + '/docs'
+      path: __dirname + '/docs',
     },
     module: {
       rules: [
@@ -49,17 +49,28 @@ module.exports = (env, argv) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
-          }
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                {
+                  plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                  ],
+                },
+              ],
+            },
+          },
         },
         {
           test: /\.html$/,
           use: [
             {
-              loader: "html-loader",
-              options: {minimize: false}
-            }
-          ]
+              loader: 'html-loader',
+              options: {minimize: false},
+            },
+          ],
         },
         {
           test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
@@ -67,26 +78,26 @@ module.exports = (env, argv) => {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }]
+              outputPath: 'fonts/',
+            },
+          }],
         },
-        scss[argv.mode]
-      ]
+        scss[argv.mode],
+      ],
     },
     plugins: [
       new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html",
-        hash: true
+        template: './src/index.html',
+        filename: './index.html',
+        hash: true,
       }),
       new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[id].css"
+        filename: '[name].css',
+        chunkFilename: '[id].css',
       }),
       new CopyWebpackPlugin([
-        {from: './src/manifest.json', to: './manifest.json'}
-      ])
-    ]
+        {from: './src/manifest.json', to: './manifest.json'},
+      ]),
+    ],
   });
 };
